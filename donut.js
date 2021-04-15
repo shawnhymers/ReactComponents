98d22faef3448a88b6add53f37eac6ad3250aca1
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2021-04-14 17:27:57 -0700

    Update donut.js
    added the css file import

--- donut.js
+++ donut.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Button} from 'react-bootstrap';
-
+import './styles.css';
 
 const HorizontalLegend = ({width, labels,color1,color2,color3,color4,color5,color6,color7,color8,color9,color10})=>{
 
